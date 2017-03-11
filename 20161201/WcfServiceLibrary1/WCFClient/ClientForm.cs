ec4dbe0b497c867da9f31a39c31c0a6c1ebbae21
Sergey Verevkin <sergeyverevkin@gmail.com>
2017-03-11 11:58:04 +0700

    old

---
+++ 20161201/WcfServiceLibrary1/WCFClient/ClientForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Windows.Forms;
+using WCFClient.ServiceReference1;
+
+namespace WCFClient
+{
+  public partial class ClientForm: Form
+  {
+    public ClientForm()
+    {
+      InitializeComponent();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string sUrlService = "http://127.0.0.1:8081/Service1";
+      BasicHttpBinding pBinding = new BasicHttpBinding();
+      EndpointAddress pEndpointAddress = new EndpointAddress(sUrlService);
+      Service1Client pClient = new ServiceReference1.Service1Client(
+        pBinding,
+        pEndpointAddress);
+      try
+      {
+        textBox1.Text = pClient.GetData((int) numericUpDown1.Value);
+      }
+      catch (Exception pE)
+      {
+        textBox1.Text = "[ERROR] " + pE.Message;
+      }
+    }
+
+    private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
---
+++ 20161201/WcfServiceLibrary1/WCFServer/ServerForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Windows.Forms;
+using WcfServiceLibrary1;
+
+namespace WCFServer
+{
+  public partial class ServerForm: Form
+  {
+    /// <summary>
+    /// Хост сервиса
+    /// </summary>
+    private ServiceHost m_pHost;
+
+    public ServerForm()
+    {
+      string sUri = "http://127.0.0.1:8081/Service1";
+      string sMetaUri = "http://127.0.0.1:8081/Service1Mex";
+      BasicHttpBinding pBinding =
+        new BasicHttpBinding();
+      pBinding.Security.Transport.ClientCredentialType = 
+        HttpClientCredentialType.None;
+      
+      ServiceMetadataBehavior pSmb = new ServiceMetadataBehavior();
+      pSmb.HttpGetEnabled = true;
+      pSmb.HttpGetUrl = new Uri(sMetaUri);
+      pSmb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+
+      m_pHost = new ServiceHost(typeof(Service1));
+      m_pHost.Description.Behaviors.Add(pSmb);
+      m_pHost.AddServiceEndpoint(
+        typeof(IService1), 
+        pBinding, 
+        sUri);
+      m_pHost.AddServiceEndpoint(
+        typeof(IMetadataExchange), 
+        MetadataExchangeBindings.CreateMexHttpBinding(), 
+        sMetaUri);
+
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      button1.Enabled = false;
+      button2.Enabled = true;
+      if (m_pHost.State != CommunicationState.Opened)
+      {
+        m_pHost.Open();      
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      button1.Enabled = true;
+      button2.Enabled = false;
+      if (m_pHost.State == CommunicationState.Opened) {
+        m_pHost.Close();
+      }
+    }
+  }
+}
